58211d562bb09efdbc01d1dab325635b8ccf022a
Jouni Roivas <jroivas@iki.fi>
2012-08-03 19:47:00 +0300

    Alternative way

--- bulls.py
+++ bulls.py
@@ -7,14 +7,6 @@ class MyRandomizer:
         """
         self._number = number
 
-    def setNumber(self, number):
-        """ Override the random feature and set the number
-        so that it's always the same
-
-        @param number Number to return (as string)
-        """
-        self._number = number
-
     def getNumber(self):
         """ Get random 4 digit number which contains only one number once.
         For example 9999 is invalid but 9876 is valid.
@@ -74,8 +66,18 @@ class BullsCowsGame:
     (0, 4)
     >>> b.bullCow("7890","0890")
     (3, 0)
+    >>> b.bullCowAlternative("7890","1234")
+    (0, 0)
+    >>> b.bullCowAlternative("7890","0914")
+    (0, 2)
+    >>> b.bullCowAlternative("7890","9876")
+    (1, 2)
+    >>> b.bullCowAlternative("7890","0987")
+    (0, 4)
+    >>> b.bullCowAlternative("7890","0890")
+    (3, 0)
     """
-    def __init__(self, randomizer=MyRandomizer()):
+    def __init__(self, randomizer=MyRandomizer(), useAlternative=False):
         """ Initialize the game and get the number to guess.
 
         @param randomize Randomizer class which should return 4 digit number (as string) when called it's getNumber() method.
@@ -96,6 +98,20 @@ class BullsCowsGame:
         bulls = sum(map(lambda x,y: 1 if (x==y) else 0, a,b))
         cows = sum(map(lambda x,y: 1 if x!=y and x in b else 0, a,b))
         return (bulls, cows)
+
+    def bullCowAlternative(self, a, b):
+        """ Alternative way to return number of bulls and cows.
+        This is powerful one liner!
+        
+        @param a Correct answer
+        @param b User's guess
+        @return Tuple containing bulls and cows
+        """
+        if len(a)!=len(b):
+            print "Invalid input!"
+            return (0,0)
+        bulls,cows = zip(*[(1,0) if i==j else (0,1) if j in a and b.count(j)==1 else (0,0) for i,j in zip(a,b)])
+        return (sum(bulls), sum(cows))
 
     def checkAnswer(self, guess):
         """ Checks whether the answer is correct.
@@ -128,7 +144,7 @@ class BullsCowsGame:
         return False
 
 if __name__=="__main__":
-    b = BullsCowsGame()
+    b = BullsCowsGame(useAlternative=True)
     while True:
         b.prompt()
         ans = sys.stdin.readline().strip()
