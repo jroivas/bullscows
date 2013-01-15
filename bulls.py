475c27e5c09c9def806bd90ce1bb9fce45e9b746
Jouni Roivas <jroivas@iki.fi>
2013-01-15 21:52:11 +0200

    Tested with Python 3, tidy a bit.

--- bulls.py
+++ bulls.py
@@ -2,7 +2,7 @@ import random
 import sys
 
 class MyRandomizer:
-    def __init__(self, number = None):
+    def __init__(self, number=None):
         """ Initialize randomize
         """
         self._number = number
@@ -13,9 +13,9 @@ class MyRandomizer:
 
         @returns Four digit number (or the overridden number) as a string
         """
-        if self._number!=None:
+        if self._number is not None:
             return self._number
-        nums = range(10)
+        nums = list(range(10))
         random.shuffle(nums)
         return "".join([str(x) for x in nums[:4]])
 
@@ -38,10 +38,10 @@ class BullsCowsGame:
     >>> b.checkAnswer("1111")
     (1, 0)
     >>> b.checkAnswer("1")
-    Invalid input!
+    Invalid input, need to be 4 digits!
     (0, 0)
     >>> b.checkAnswer("11234")
-    Invalid input!
+    Invalid input, need to be 4 digits!
     (0, 0)
     >>> b.handleGuess("1234")
     Correct!
@@ -56,25 +56,25 @@ class BullsCowsGame:
     True
     >>> b.prompt()
     Your guess:
-    >>> b.bullCow("7890","1234")
+    >>> b.bullCow("7890", "1234")
     (0, 0)
-    >>> b.bullCow("7890","0914")
+    >>> b.bullCow("7890", "0914")
     (0, 2)
-    >>> b.bullCow("7890","9876")
+    >>> b.bullCow("7890", "9876")
     (1, 2)
-    >>> b.bullCow("7890","0987")
+    >>> b.bullCow("7890", "0987")
     (0, 4)
-    >>> b.bullCow("7890","0890")
+    >>> b.bullCow("7890", "0890")
     (3, 0)
-    >>> b.bullCowAlternative("7890","1234")
+    >>> b.bullCowAlternative("7890", "1234")
     (0, 0)
-    >>> b.bullCowAlternative("7890","0914")
+    >>> b.bullCowAlternative("7890", "0914")
     (0, 2)
-    >>> b.bullCowAlternative("7890","9876")
+    >>> b.bullCowAlternative("7890", "9876")
     (1, 2)
-    >>> b.bullCowAlternative("7890","0987")
+    >>> b.bullCowAlternative("7890", "0987")
     (0, 4)
-    >>> b.bullCowAlternative("7890","0890")
+    >>> b.bullCowAlternative("7890", "0890")
     (3, 0)
     """
     def __init__(self, randomizer=MyRandomizer(), useAlternative=False):
@@ -93,11 +93,11 @@ class BullsCowsGame:
         @param b User's guess
         @return Tuple containing bulls and cows
         """
-        if len(a)!=len(b):
-            print "Invalid input!"
+        if len(a) != len(b):
+            print ("Invalid input, need to be %s digits!" % (len(a)))
             return (0,0)
-        bulls = sum(map(lambda x,y: 1 if (x==y) else 0, a,b))
-        cows = sum(map(lambda x,y: 1 if x!=y and x in b else 0, a,b))
+        bulls = sum(map(lambda x,y: 1 if (x == y) else 0, a, b))
+        cows = sum(map(lambda x,y: 1 if x != y and x in b else 0, a, b))
         return (bulls, cows)
 
     def bullCowAlternative(self, a, b):
@@ -108,10 +108,11 @@ class BullsCowsGame:
         @param b User's guess
         @return Tuple containing bulls and cows
         """
-        if len(a)!=len(b):
-            print "Invalid input!"
-            return (0,0)
-        bulls,cows = zip(*[(1,0) if i==j else (0,1) if j in a and b.count(j)==1 else (0,0) for i,j in zip(a,b)])
+        if len(a) != len(b):
+            print ("Invalid input, need to be %s digits!" % (len(a)))
+            return (0, 0)
+
+        bulls,cows = zip(*[(1,0) if i == j else (0, 1) if j in a and b.count(j) == 1 else (0, 0) for i, j in zip(a, b)])
         return (sum(bulls), sum(cows))
 
     def checkAnswer(self, guess):
@@ -120,19 +121,23 @@ class BullsCowsGame:
         @param guess User's guess
         @return True if right, otherwise bulls and cows count
         """
-        if self._number == guess: return True
+        if self._number == guess:
+            return True
 
         if self._useAlternative:
             (bulls, cows) = self.bullCowAlternative(self._number, guess)
         else:
             (bulls, cows) = self.bullCow(self._number, guess)
-        if bulls==len(self._number): return True
+
+        if bulls == len(self._number):
+            return True
+
         return (bulls, cows)
 
     def prompt(self):
         """ Prompt user
         """
-        print "Your guess:"
+        print ("Your guess:")
 
     def handleGuess(self, guess):
         """ Handle a user guess
@@ -141,10 +146,10 @@ class BullsCowsGame:
         @return True if correct, False otherwise
         """
         res = self.checkAnswer(guess)
-        if res == True:
-            print "Correct!"
+        if res is True:
+            print ("Correct!")
             return True
-        print "Bulls: %s, Cows: %s"%res
+        print ("Bulls: %s, Cows: %s" % res)
         return False
 
 if __name__=="__main__":
@@ -152,7 +157,7 @@ if __name__=="__main__":
     while True:
         b.prompt()
         ans = sys.stdin.readline().strip()
-        if ans == None or ans == "":
+        if ans is None or ans == "":
             sys.exit(0)
         if b.handleGuess(ans):
             sys.exit(0)
